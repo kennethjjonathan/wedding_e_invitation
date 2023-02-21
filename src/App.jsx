71a33219e9622029fdaf625e293f7a2a624c5510
kennethjjonathan <106253255+kennethjjonathan@users.noreply.github.com>
2023-02-21 18:32:30 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import picture from "./assets/pictures/myProject.jpg"
+
+function App() {
+  
+
+  return (
+    <div className="hero-container">
+      <div className='hero-titles'>
+        <div>The Wedding Of</div>
+        <h1>LEO & DESY</h1>
+        <h3>01 • JULY • 2023</h3>
+        <button>View Invitation Details</button>
+      </div>
+      <div>
+        
+      </div>
+    </div>
+  )
+}
+
+export default App
