78df2880ce77909932172e9e21276092e5a0f939
kennethjjonathan <106253255+kennethjjonathan@users.noreply.github.com>
2023-02-22 01:18:08 +0700

    Adding invitation details

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,20 @@ function App() {
     <div className="hero-container">
       <div className='hero-titles'>
         <div>The Wedding Of</div>
-        <h1>LEO & DESY</h1>
-        <h3>01 • JULY • 2023</h3>
+        <h1>LEO</h1>
+        <h3>&</h3>
+        <h1>DESY</h1>
+        <h3>22 • OCTOBER • 2022</h3>
         <button>View Invitation Details</button>
+      </div>
+      <div className='invitation-details'>
+        <p className="invitation-details-opening">We warmly request the pleasure of your company at:</p>
+        <h2>WEDDING RECEPTION</h2>
+        <h3>HABITATE JAKARTA</h3>
+        <p>Jl. Setia Budi Utara Raya No. 5, Kuningan, Karet Kuningan, Jakarta, Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta 12920</p>
+        <button>Show at Google Maps</button>
+        <h3>Saturday, 22 October 2022</h3>
+        <h3>At: 17:30-19:30</h3>
       </div>
       <div>
         
