5af51c835047366125b82561e8b8baf420f47048
kennethjjonathan <106253255+kennethjjonathan@users.noreply.github.com>
2023-02-23 17:19:06 +0700

    add scroll to, rel noopener noreferrer, and tidying up some code

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,37 @@
-import { useState } from 'react'
+import { useRef } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import picture from "./assets/pictures/myProject.jpg"
 
 function App() {
   
+  const invitationDetails = useRef(null);
+
+  const scrollToSection = (ref) => {
+    window.scrollTo({
+      top: ref.current.offsetTop,
+      behavior: "smooth"
+    })
+  }
 
   return (
-    <div className="hero-container">
-      <section className='hero-titles'>
-        <div>The Wedding Of</div>
-        <h1>LEO</h1>
-        <h3>&</h3>
-        <h1>DESY</h1>
-        <h3>22 • OCTOBER • 2022</h3>
-        <button>View Invitation Details</button>
+    <div>
+      <section className='hero'>
+        <div className='hero-titles'>
+          <h2>The Wedding Of</h2>
+          <h1>LEO</h1>
+          <h3>&</h3>
+          <h1>DESY</h1>
+          <h3>22 • OCTOBER • 2022</h3>
+          <button onClick={() => scrollToSection(invitationDetails)}>View Invitation Details</button>
+        </div>
       </section>
-      <section className='invitation-details'>
+      <section ref={invitationDetails} className='invitation-details'>
         <p className="invitation-details-opening">We warmly request the pleasure of your company at:</p>
         <div className="invitation-details-place">
           <h2>HABITATE JAKARTA</h2>
           <p>Jl. Setia Budi Utara Raya No. 5, Kuningan, Karet Kuningan, Jakarta, Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta 12920</p>
-          <a href="https://goo.gl/maps/GrF1xpXTWiYcBvbo7" target="_blank"><button>View in Google Maps</button></a>
+          <a href="https://goo.gl/maps/GrF1xpXTWiYcBvbo7" target="_blank" rel="noopener noreferrer"><button>View in Google Maps</button></a>
         </div>
         <div className="invitation-details-date">
           <h2>Saturday, 22 October 2022</h2>
