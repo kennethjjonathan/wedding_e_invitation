48665328d1d8906560676dc8d79374264cfd8a6f
kennethjjonathan <106253255+kennethjjonathan@users.noreply.github.com>
2023-02-26 16:41:45 +0700

    Minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useRef } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import picture from "./assets/pictures/myProject.jpg"
 
 function App() {
   
@@ -19,10 +18,10 @@ function App() {
       <section className='hero'>
         <div className='hero-titles'>
           <h2>The Wedding Of</h2>
-          <h1>LEO</h1>
+          <h1>SHREK</h1>
           <h3>&</h3>
-          <h1>DESY</h1>
-          <h3>22 • OCTOBER • 2022</h3>
+          <h1>FIONA</h1>
+          <h3>22 • FEBRUARY • 2023</h3>
           <button onClick={() => scrollToSection(invitationDetails)}>View Invitation Details</button>
         </div>
       </section>
@@ -34,7 +33,7 @@ function App() {
           <a href="https://goo.gl/maps/GrF1xpXTWiYcBvbo7" target="_blank" rel="noopener noreferrer"><button>View in Google Maps</button></a>
         </div>
         <div className="invitation-details-date">
-          <h2>Saturday, 22 October 2022</h2>
+          <h2>Saturday, 22 FEBRUARY 2023</h2>
           <h3>Holy Matrimony at: 15:30-16:30</h3>
           <h3>Reception Ceremony at: 17:30-19:30</h3>
         </div>
