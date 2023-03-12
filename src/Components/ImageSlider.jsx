0b89508a75214e30c32e0ab5173a0a5c46cc0f97
kennethjjonathan <106253255+kennethjjonathan@users.noreply.github.com>
2023-03-13 01:24:16 +0700

    gallery image slider

--- src/Components/ImageSlider.jsx
+++ src/Components/ImageSlider.jsx
@@ -1,20 +1,37 @@
 import React, { useState } from "react";
 import { SliderData } from "./SliderData";
-import { FaBeer } from 'react-icons/fa';
+import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from 'react-icons/fa';
 
 
 const ImageSlider = () => {
     const [current, setCurrent] = useState(0);
     const length = SliderData.length;
 
+    const nextSlide = () => {
+        setCurrent(current === length - 1 ? 0 : current + 1);
+    };
+
+    const prevSlide = () => {
+        setCurrent(current === 0 ? length - 1 : current - 1)
+    }
+
+    if (!Array.isArray(SliderData) || length <= 0) {
+        return null;
+    }
+
     return (
-        <div>
-            <FaBeer />
-            <button className="left-button">previous</button>
-            <button className="right-button">next</button>
+        <div className="gallery-slider">
+            <FaArrowAltCircleLeft className="left-arrow" onClick={prevSlide} />
+            <FaArrowAltCircleRight className="right-arrow" onClick={nextSlide} />
             {SliderData.map((slide, index) => {
-                return <img src={slide.image} alt="Marriage Photos" />
-            })};
+                return (
+                    <div className={index === current ? "active-slide" : "inactive-slide"} key={index}>
+                        {index === current && (
+                            <img src={slide.image} alt="Marriage Photos" className="gallery-slider-images" />
+                        )}
+                    </div>
+                )
+            })}
         </div>
     )
 }
